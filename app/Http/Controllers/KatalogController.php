15bc8d4f1eb21b7eba4cb1a0f1061927f600d667
TogaSetiawan <atosna70@gmail.com>
2025-06-19 11:24:22 +0700

    fix toga

--- app/Http/Controllers/KatalogController.php
+++ app/Http/Controllers/KatalogController.php
@@ -15,25 +15,23 @@ class KatalogController extends Controller
 
     public function hitungDiskon(Request $request)
     {
-        // 1. Validasi input untuk memastikan data yang masuk sesuai
         $validated = $request->validate([
             'harga_satuan' => 'required|numeric|min:0',
             'jumlah_pembelian' => 'required|integer|min:1',
         ]);
 
-        // 2. Ambil data dari request yang sudah divalidasi
         $satuan_harga = $validated['harga_satuan'];
         $pembelian_jumlah = $validated['jumlah_pembelian'];
         $total_harga_awal = $harga_satuan * $jumlah_pembelian;
         
-        $persentase_diskon = 0;
+        $persen_diskon = 0;
 
         if ($pembelian_jumlah % 500 == 0) {
-            $persentase_diskon = 50; 
+            $persen_diskon = 50; 
         } elseif ($pembelian_jumlah % 100 == 0) {
-                       $persentase_diskon = 0;
+                       $persen_diskon = 0;
         } elseif ($pembelian_jumlah % 40 == 0) {
-            $persentase_diskon = 10;
+            $persen_diskon = 10;
         }
         $nilai_diskon = $total_harga_awal * ($persentase_diskon / 100);
         $harga_akhir = $total_harga_awal - $nilai_diskon;
@@ -43,7 +41,7 @@ class KatalogController extends Controller
                 'harga_satuan' => $satuan_harga,
                 'jumlah_pembelian' => $pembelian_jumlah,
                 'total_harga_awal' => $total_harga_awal,
-                'persentase_diskon' => $persentase_diskon,
+                'persen_diskon' => $persen_diskon,
                 'nilai_diskon' => $nilai_diskon,
                 'harga_akhir' => $harga_akhir,
             ]
